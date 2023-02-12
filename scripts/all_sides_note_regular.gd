c34d3d60fede38a39d7615b26be6e1e73cd5e7e4
jue131 <83157668+jue131@users.noreply.github.com>
2023-02-12 18:25:23 +0900

    properly working note blocks

---
+++ scripts/all_sides_note_regular.gd
@@ -0,0 +1,17 @@
+extends "res://modules/note-blocks/scripts/regular_note.gd"
+
+@export var left_push_velocity: float = -350
+@export var right_push_velocity: float = 350
+
+func _physics_process(delta: float) -> void:
+	super(delta)
+	
+	if cast_left && is_player_colliding(cast_left):
+		player.velocity_local.x = left_push_velocity
+		bump(false, 90, true)
+		return
+	
+	if cast_right && is_player_colliding(cast_right):
+		player.velocity_local.x = right_push_velocity
+		bump(false, 270, true)
+		return
--- scripts/regular_note.gd
+++ scripts/regular_note.gd
@@ -14,25 +14,30 @@ var hit_from_above = false
 
 func _physics_process(delta: float) -> void:
 	delta = Thunder.get_delta(delta)
-	if Input.is_action_just_pressed("m_jump"):
-		jump_timer = 8
-	if Input.is_action_just_released("m_jump"):
-		jump_timer = 0
 	
-	if jump_timer > 0: jump_timer -= delta
-	
-	if is_player_colliding(cast_above):
-		player.states.set_state("jump")
-		if jump_timer > 0:
-			player.velocity_local.y = strong_jump_velocity * player.config.stomp_multiplicator
-		else:
-			player.velocity_local.y = weak_jump_velocity * player.config.stomp_multiplicator
+	if cast_above:
+		if Input.is_action_just_pressed("m_jump"):
+			jump_timer = 8
+		if Input.is_action_just_released("m_jump"):
+			jump_timer = 0
 		
-		hit_from_above = true
-		bump(false, 180, true)
-		return
+		if jump_timer > 0: jump_timer -= delta
+		
+		if is_player_colliding(cast_above):
+			if player.global_position.rotated(player.global_rotation).y > cast_above.global_position.rotated(global_rotation).y:
+				player.global_position -= Vector2(0, 1).rotated(global_rotation)
+			player.states.set_state("jump")
+			if jump_timer > 0:
+				player.velocity_local.y = strong_jump_velocity * player.config.stomp_multiplicator
+			else:
+				player.velocity_local.y = weak_jump_velocity * player.config.stomp_multiplicator
+				
+			
+			hit_from_above = true
+			bump(false, 180, true)
+			return
 	
-	if is_player_colliding(cast_below) && player.velocity_local.y <= 50 && !player.is_on_floor():
+	if cast_below && is_player_colliding(cast_below) && player.velocity_local.y <= 50 && !player.is_on_floor():
 		bump(false)
 		return
 
